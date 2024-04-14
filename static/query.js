2d8928b9f4384d6a4f1ac5f7a5f9b0cc0a829551
Islamoflix <164672885+islamoflix@users.noreply.github.com>
2024-04-14 22:00:18 +0530

    search query update

--- static/query.js
+++ static/query.js
@@ -3,38 +3,56 @@ const suggestions = document.querySelector('#suggestions');
 const searchArea = document.querySelector('.search-area');
 
 
-const index = {
-    home : 'https://islamoflix.github.io/template/index.html',
-    "pillars of islam" : 'https://islamoflix.github.io/template/pillars.html',
-    "faith in islam": "https://islamoflix.github.io/template/faith.html",
-    "sharia law": "https://islamoflix.github.io/template/sharia.html",
-    "prophet Muhmmad(S.A.W)": "https://islamoflix.github.io/template/prophet.html",
-    "concept of allah/god": "https://islamoflix.github.io/template/allah.html",
-    "agenda of Islam": "https://islamoflix.github.io/template/agenda.html",
-    "refutations": "https://islamoflix.github.io/template/refutations/refutations.html",
-    "revert Section": "https://islamoflix.github.io/template/revert-section/revert-section.html"
-}
-let query = "";
 
-function linearSearch(index, query){
-    let searchResults = [];
-    for(let i in index){
-        console.log(`index ${i} and query ${query}`);
+const index = {
+    Home : 'https://islamoflix.github.io/template/index.html',
+    "Pillars of Islam" : 'https://islamoflix.github.io/template/pillars.html',
+    "Faith in Islam": "https://islamoflix.github.io/template/faith.html",
+    "Sharia law": "https://islamoflix.github.io/template/sharia.html",
+    "Prophet Muhmmad(S.A.W)": "https://islamoflix.github.io/template/prophet.html",
+    "Concept of allah/god": "https://islamoflix.github.io/template/allah.html",
+    "Agenda of Islam": "https://islamoflix.github.io/template/agenda.html",
+    "Refutations": "https://islamoflix.github.io/template/refutations/refutations.html",
+    "Revert Section": "https://islamoflix.github.io/template/revert-section/revert-section.html",
+}
+
+let key = Object.keys(index); //stores keys of index obj as an array for ease to access keys by indexing
+const keywords = {
+    "home, Home, main, Main" : key[0],
+    "pillars of islam, , pillras of islam, Pillars of islam, Pillars of Islam, pillars in islam, Pillars in islam" : key[1],
+    "faith in islam, Faith in islam, FAITH IN ISLAM": key[2],
+    "sharia law, sharia in islam, Sharia law": key[3],
+    "muhammad, Muhammad, muhmmad, prophet, saw, SAW, prophet muhammad, Prophet Muhammad": key[4],
+    "god, Allah, concept of god, concept of allah, Concept of god, Concept of allah, Concept of Allah": key[5],
+    "agenda of islam, Agenda of islam, what is islam, why islam, Islam": key[6],
+    "Refutations, refutations, answers, solutions, Solutions, Why?, why?": key[7],
+    "revert section, Revert section, Revert Sections, reverts, Reverts, materials, Materials": key[8]
+}
+
+let query = "";
+// not very efficient but data is low so linear search will not consume much time
+function linearSearch(query){
+    let searchResults = [];//stores search results in an array based on keywords
+    for(let i in keywords){
         if(query == ''){
-            searchResults.splice(0);
+            searchResults.splice(0);    //is there is no query or the query got cleared, the prev results deletes
         }else if(i.includes(query)){
-            searchResults.push(i);
+            searchResults.push(keywords[i]);
         }
     }
     return searchResults;
 }
 
+
+//clear all the search on suggestions dialog
 function clearSearch(element){
     while(element.firstChild){
         element.removeChild(element.firstChild);
     }
 }
 
+
+//display suggestions on suggestions dialog
 function displaySearch(searchResults){
     if(searchResults.length === 0){
         clearSearch(suggestions);
@@ -52,8 +70,7 @@ function displaySearch(searchResults){
 
 search.addEventListener('input', () => {
     query = search.value.trim();
-    let searchResults = linearSearch(index, query);
-    console.log(`${searchResults} length ${searchResults.length} query for ${search.value}`);
-    clearSearch(suggestions);
-    displaySearch(searchResults);
+    let searchResults = linearSearch(query); //searchResults comes as an array
+    clearSearch(suggestions);  //clear old search when new query come
+    displaySearch(searchResults); //array of search results display row by row as block
 });
